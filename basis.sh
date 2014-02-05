4d31be4244b554625ef9773dd2444cb142daa668
Niels Doorn <nielsdoorn@gmail.com>
2014-02-05 07:01:16 +0100

    added animation loop

--- basis.sh
+++ basis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 git clone https://github.com/nielsdoorn/basis.git .
+git clone https://gist.github.com/1579671.git public/libs/
 curl -s -o public/css/normalize.css https://raw2.github.com/necolas/normalize.css/master/normalize.css
 curl -s -o public/libs/jquery.js http://code.jquery.com/jquery.min.js
 npm install
-npm start &
-sleep 2
-open http://localhost:8080
+open public/index.html
