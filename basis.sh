6617bd1b9415fde9a4ba3ad054686e80736d57fc
Niels Doorn <nielsdoorn@gmail.com>
2014-02-08 13:47:01 +0100

    added mocha and a test

--- basis.sh
+++ basis.sh
@@ -4,4 +4,5 @@ git clone https://gist.github.com/1579671.git public/libs/
 curl -s -o public/css/normalize.css https://raw2.github.com/necolas/normalize.css/master/normalize.css
 curl -s -o public/libs/jquery.js http://code.jquery.com/jquery.min.js
 npm install
+npm install mocha
 open public/index.html
